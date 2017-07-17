2617091c16352f5f5bad76fd012c9243a6b5a591
Alexander Tiedmann <alexander@tiedmann.berlin>
2017-07-17 18:38:18 +0200

    Hinzufuegen der Grafiken

--- Uebung13-LineareRegression.m
+++ Uebung13-LineareRegression.m
@@ -69,27 +69,62 @@ fprintf("-----Aufgabe 1----- \n")
 ym=[8.6 11.5 12.4 15.6 15.1 17.7 18.9 18.6 21.3 24.3];
 xm=[10.6 14.0 18.1 23.2 25.0 26.4 30.5 32.5 36.6 40.1];
 [a,b]=linReg(xm,ym)
+%Ausgabe
+IntMin=[min(ym),min(xm)];
+IntMax=[max(ym),max(xm)];
+intervall=[min(IntMin):0.1:max(IntMax)];
+func=@(xm)a.*xm.+b;
+y=func(intervall);
+figure('Name',"Aufgabe 13",'NumberTitle','off'), 
+subplot(2,3,1),line(intervall,y),line(xm,ym,"linestyle","none","marker","*","color",'r'),title("13.1")
+
 
 %--------Aufgabe2--------------------------
 fprintf("-----Aufgabe 2----- \n")
 xm=[1.3 2.2 2.9 3.1 4.5 5.7 7.1 8.0 8.7 8.9 9.3 9.9 ];
 ym=[1.3 1.0 0.85 0.80 0.33 0 -0.4 -0.7 -0.9 -1.0 -1.1 -1.2];
 [a,b]=linReg(xm,ym)
+%Ausgabe
+IntMin=[min(ym),min(xm)];
+IntMax=[max(ym),max(xm)];
+intervall=[min(IntMin):0.1:max(IntMax)];
+func=@(xm)a.*xm.+b;
+y=func(intervall);
+subplot(2,3,2),line(intervall,y),line(xm,ym,"linestyle","none","marker","*","color",'r'),title("13.2")
 
 %--------Aufgabe3--------------------------
 fprintf("-----Aufgabe 3----- \n")
 xm=[1 2 3 3.5 4.5 5.5 6 7 8 8.5 9.5 10];
 ym=[11 7.3 4.8 4.1 1.0 0 0.6 2 3.7 4.2 5.6 8];
 [a,b,c]=quadReg(xm,ym)
+%Ausgabe
+IntMin=[min(ym),min(xm)];
+IntMax=[max(ym),max(xm)];
+intervall=[min(IntMin):0.1:max(IntMax)];
+func=@(xm)a.*xm.^2.+b.*xm.+c;
+y=func(intervall);
+subplot(2,3,3),line(intervall,y),line(xm,ym,"linestyle","none","marker","*","color",'r'),title("13.3")
 
 %--------Aufgabe4--------------------------
 fprintf("-----Aufgabe 4----- \n")
 ym=[2.63 1.18 1.16 1.54 2.65 5.41 7.67];
 xm=[0.04 0.32 0.51 0.73 1.03 1.42 1.60];
 [a,b,c]=quadReg(xm,ym)
+%Ausgabe
+intervall=[min(xm):0.1:max(xm)+1];
+func=@(xm)a.*xm.^2.+b.*xm.+c;
+y=func(intervall);
+subplot(2,3,4),line(intervall,y),line(xm,ym,"linestyle","none","marker","*","color",'r'),title("13.4")
 
 %--------Aufgabe5--------------------------
 fprintf("-----Aufgabe 5----- \n")
 xm=[1 2 3 4 5];
 ym=[1.8395 0.6765 0.2490 0.0915 0.0335];
 [a,b]=regression(xm,ym)
+%Ausgabe
+IntMin=[min(ym),min(xm)];
+IntMax=[max(ym),max(xm)];
+intervall=[min(IntMin):0.1:max(IntMax)];
+func=@(xm)a.*exp(b.*xm);
+y=func(intervall);
+subplot(2,3,5),line(intervall,y),line(xm,ym,"linestyle","none","marker","*","color",'r'),title("13.5")
